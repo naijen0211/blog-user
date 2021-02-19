9da245711254381036f9e994d8305cb74ff15eea
naijen0211 <41713241+naijen0211@users.noreply.github.com>
2021-02-18 19:08:20 -0800

    Update main.py

--- blog-with-users-start/main.py
+++ blog-with-users-start/main.py
@@ -15,7 +15,7 @@ import os
 
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = os.environ,get("SECRET_KEY")
+app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
 ckeditor = CKEditor(app)
 Bootstrap(app)
 
