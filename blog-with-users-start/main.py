5d66675c2df3dc0a8c783f499a9c01e8950aef84
naijen0211 <41713241+naijen0211@users.noreply.github.com>
2021-02-18 19:10:47 -0800

    Update main.py

--- blog-with-users-start/main.py
+++ blog-with-users-start/main.py
@@ -20,7 +20,7 @@ ckeditor = CKEditor(app)
 Bootstrap(app)
 
 ##CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///blog.db"
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL", "sqlite:///blog.db")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
@@ -72,7 +72,7 @@ gravatar = Gravatar(app,
                     use_ssl=False,
                     base_url=None)
 
-#db.create_all()
+db.create_all()
 
 login_manager = LoginManager()
 login_manager.init_app(app)
